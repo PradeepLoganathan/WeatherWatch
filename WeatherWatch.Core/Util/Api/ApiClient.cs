0a09a8bc1af105d213aea8d7aff43f73dab79e6b
Pradeep <Pradeep@THEALIEN>
2016-10-17 14:15:33 +0530

    First commit.

---
+++ WeatherWatch.Core/Util/Api/ApiClient.cs
@@ -0,0 +1,49 @@
+﻿#region
+
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+#endregion
+
+namespace WeatherNet.Util.Api
+{
+    public class ApiClient
+    {
+        public static string API_URL = "http://api.openweathermap.org/data/2.5";
+        public static void ProvideApiKey(string apiKey)
+        {
+            api_key = apiKey;
+        }
+        static string api_key;
+        public static string API_KEY 
+        { 
+            get
+            {
+                if (string.IsNullOrEmpty(api_key))
+                {
+                    var key = ConfigurationManager.AppSettings["APIKey"];
+                    if (string.IsNullOrEmpty(key))
+                        throw new Exception("API key not found");
+
+                    api_key = key;
+                }
+
+                return api_key;
+            }
+        }
+
+        public static JObject GetResponse(String url)
+        {
+            
+            using (var client = new WebClient())
+            {
+                //Trace.WriteLine("<HTTP - GET - " + url + " >");
+                var response = client.DownloadString(string.Format("{0}{1}&appid={2}", API_URL, url, API_KEY));
+                var parsedResponse = JObject.Parse(response);
+                return parsedResponse;
+            }
+        }
+    }
+}
