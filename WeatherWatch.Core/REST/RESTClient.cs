1c025c52b621d232b40cb58d7145f3be2432d685
Pradeep <Pradeep@THEALIEN>
2016-10-17 19:48:10 +0530

    Working on the Core REST request and client classes.

---
+++ WeatherWatch.Core/REST/RESTClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WeatherWatch.Core.REST
+{
+    internal class RESTClient
+    {
+        private int _MaxRetryCount;
+
+        internal JsonSerializer Serializer { get; set; }
+        internal Uri BaseURL { get; set; }
+        internal List<KeyValuePair<string, string>> QueryString { get; }
+        internal Encoding Encoding { get; }
+        
+
+        public int MaxRetryCount
+        {
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException();
+                else
+                    _MaxRetryCount = value;
+            }
+            get
+            {
+                return _MaxRetryCount;
+            }
+        }
+
+        public void AddQueryString(string Key, string Value)
+        {
+            QueryString.Add(new KeyValuePair<string, string>(Key, Value));
+        }
+
+
+    }
+}
---
+++ WeatherWatch.Core/REST/RestRequest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherWatch.Core.REST
+{
+    internal class RESTRequest
+    {
+        private readonly RESTClient _client;
+        private readonly string _endpoint;
+        private List<KeyValuePair<string,string>> _QueryString;
+        private List<KeyValuePair<string, string>> _URLSegment;
+        private object _body;
+
+        public RESTRequest(RESTClient client, string EndPoint)
+        {
+            _client = client;
+            _endpoint = EndPoint;
+        }
+
+        public RESTRequest AddParameter(KeyValuePair<string, string> pair, ParameterType pType = ParameterType.QueryString)
+        {
+            AddParameter(pair.Key, pair.Value, pType);
+            return this;
+        }
+
+        public RESTRequest AddParameter(string Key, string Value, ParameterType pType = ParameterType.QueryString)
+        {
+            switch (pType)
+            {
+                case ParameterType.QueryString:
+                    return AddQueryString(Key, Value);
+                case ParameterType.URLSegment:
+                    return AddURLSegment(Key, Value);
+            }
+            return this;
+
+        }
+
+        public RESTRequest AddQueryString(string Key, string Value)
+        {
+            if (_QueryString == null)
+            {
+                _QueryString = new List<KeyValuePair<string, string>>();
+                _QueryString.Add(new KeyValuePair<string, string>(Key, Value));
+            }
+            else
+            {
+                _QueryString.Add(new KeyValuePair<string, string>(Key, Value));
+            }
+
+            return this;
+        }
+
+        public RESTRequest AddURLSegment(string Key, string Value)
+        {
+            if (_URLSegment == null)
+            {
+                _URLSegment = new List<KeyValuePair<string, string>>();
+                _URLSegment.Add(new KeyValuePair<string, string>(Key, Value));
+            }
+            else
+            {
+                _URLSegment.Add(new KeyValuePair<string, string>(Key, Value));
+            }
+
+            return this;
+        }
+
+        private HttpRequestMessage PrepareRequest()
+        {
+            StringBuilder sbQueryString = new StringBuilder();
+
+            if (_QueryString != null)
+            {
+                foreach (KeyValuePair<string, string> pair in _QueryString)
+                    AppendQueryString(sbQueryString, pair);
+            }
+
+            
+
+        }
+
+        private void AppendQueryString(StringBuilder sbQueryString, KeyValuePair<string, string> pair)
+        {
+            if (sbQueryString.Length > 0)
+            {
+                sbQueryString.Append("&");
+                sbQueryString.Append(WebUtility.UrlEncode(pair.Key));
+                sbQueryString.Append("=");
+                sbQueryString.Append(WebUtility.UrlEncode(pair.Value));
+
+            }
+        }
+
+        private async Task<HttpResponseMessage> SendInternal(HttpMethod Method)
+        {
+            int _timestotry = _client.MaxRetryCount + 1;
+
+            do
+            {
+
+            } while (_timestotry-- > 0);
+        }
+
+
+    }
+}
